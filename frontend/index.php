431ab47fd6374d55a121d27b0985b25344f0a0c2
singepantsu <mekki99@hotmail.fr>
2022-04-01 09:35:43 +0200

    change html to php

--- frontend/index.php
+++ frontend/index.php
@@ -42,9 +42,9 @@
         <!-- Masthead-->
         <header class="masthead">
             <div class="container">
-                <div class="masthead-subheading">Welcome To Our Studio!</div>
-                <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
-                <a class="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
+                <div class="masthead-subheading">Welcome, dear user!</div>
+                <div class="masthead-heading text-uppercase">IMangerMieux</div>
+                <a class="btn btn-primary btn-xl text-uppercase" href="#services">Connexion</a>
             </div>
         </header>
         <!-- Services-->
